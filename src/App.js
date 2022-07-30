0d4f9c7ef87e0252814559763fe4ac72157576ae
md <mdfiroz811315@gmail.com>
2022-07-30 16:28:10 +0530

    code

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import StopWatch from './components/stopwatch/stopwatch';
+
+function App() {
+return (
+	<div className="App">
+		stopwatch
+	<StopWatch/>
+	</div>
+);
+}
+export default App;
